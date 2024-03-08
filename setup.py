7c352b6780587d250428d39670cdc79529088764
piidus <sudiipkumarbasu@gmail.com>
2024-03-08 15:26:58 +0530

    project will add to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='financial_year',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='It Calculate financial year related dates',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/piidus/FinancialYear',
+    author='Sudiip',
+    author_email='sudiipkumarbasu@gmail.com',
+)
