2e3068ff5b8b1923d0522831bb10978f8ab7b165
piidus <sudiipkumarbasu@gmail.com>
2024-03-16 18:42:57 +0530

    3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='financialyear',
-    version='0.3.7',
+    version='0.3.8',
     packages=find_packages(),
     install_requires=[],
     description='It Calculate financial year related dates',
